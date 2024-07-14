88d0e3b5bfdbe2b6049c9cf7cc9dea266965926a
Hemesh <112506925+Hemeshdev@users.noreply.github.com>
2024-07-14 18:38:45 +0530

    Create 2410. Maximum Matching of Players With Trainers.cpp

---
+++ Biweekly Contest 87/2410. Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int n = players.size();
+        int m = trainers.size();
+        int i = 0;
+        int j = 0;
+        while(i<n && j<m){
+            if(players[i]<=trainers[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
